10799cc1479d82d99b0f9d5ed7770ec52ba7e859
Aditya Lekhi <adityalekhi777@gmail.com>
2025-01-28 17:18:53 +0530

    Added Redux Store logic

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,44 +1,42 @@
-'use client';
+"use client"
 
-import { Box, Flex, Spacer, Button, Text } from '@chakra-ui/react';
-import Link from 'next/link';
+import { Box, Flex, Spacer, Button, Text } from "@chakra-ui/react"
+import NextLink from "next/link"
 
 export default function Navbar() {
-  const isAuthenticated = true;
+ 
+  let isAuthenticated = true;
 
   return (
-    <Box bg='blue.500' px={4} py={3}>
-      <Flex alignItems='center'>
-        <Link href='/' passHref>
-          <Text fontSize='xl' fontWeight='bold' color='white' cursor='pointer'>
+    <Box bg="blue.500" px={4} py={3}>
+      <Flex alignItems="center">
+        <NextLink href="/" passHref>
+          <Text fontSize="xl" fontWeight="bold" color="white" cursor="pointer">
             Desi Taste
           </Text>
-        </Link>
+        </NextLink>
         <Spacer />
         <Box>
-          <Link href='/' passHref>
-            <Button as='a' variant='ghost' color='white' mr={2}>
-              Home
-            </Button>
-          </Link>
-          <Link href='/cart' passHref>
-            <Button as='a' variant='ghost' color='white' mr={2}>
+          <Button as={NextLink} href="/" variant="ghost" color="white" mr={2}>
+            Home
+          </Button>
+          {isAuthenticated && (
+            <Button as={NextLink} href="/cart" variant="ghost" color="white" mr={2}>
               Cart
             </Button>
-          </Link>
+          )}
           {isAuthenticated ? (
-            <Button variant='outline' color='white'>
+            <Button  variant="outline" color="white">
               Logout
             </Button>
           ) : (
-            <Link href='/login' passHref>
-              <Button as='a' variant='outline' color='white'>
-                Login
-              </Button>
-            </Link>
+            <Button as={NextLink} href="/login" variant="outline" color="white">
+              Login
+            </Button>
           )}
         </Box>
       </Flex>
     </Box>
-  );
+  )
 }
+
---
+++ store.js
@@ -0,0 +1,14 @@
+"use client"
+
+import { configureStore } from "@reduxjs/toolkit"
+import cartReducer from "./slices/cartSlice"
+import authReducer from "./slices/authSlice"
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    auth: authReducer,
+  },
+})
+
+export default store
